a41f532016eb473aa3d9a7683e7611c89a8f11b7
Leah Grace <Leahgdg@hotmail.com>
2023-02-01 12:04:01 -0500

    initialized q5 template

---
+++ CourseWork/wk4/hw4_q5.cpp
@@ -0,0 +1,15 @@
+//This program will prompt the user for a positive integer, n
+//the program will print a tectual image of an hourglass made of 2n lines of asterisks
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int userInteger = 0;
+
+
+
+    return 0;
+}
