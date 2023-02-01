98aa88496fbcda51773be396ac8bc69d7b317617
Leah Grace <Leahgdg@hotmail.com>
2023-02-01 12:41:16 -0500

    redetermine loop

--- CourseWork/wk4/hw4_q5.cpp
+++ CourseWork/wk4/hw4_q5.cpp
@@ -8,8 +8,43 @@ using namespace std;
 int main(){
 
     int userInteger = 0;
+    cout<<"Enter a positive integer"<<endl;
+    cin>>userInteger;
+
+    int rowCounter = 1;
+    int starCounter = userInteger;
 
 
+    for (rowCounter; rowCounter <= userInteger; rowCounter++){
 
+        for(starCounter; starCounter > 0; starCounter--){
+            cout<<"*";
+        }
+
+        cout<<endl;
+
+        starCounter = userInteger - rowCounter;
+
+    }
+
+
+//At this point the 
+//row counter is equal to the userInput+1
+//starCounter is equal to 0
+//UserInput is unchanged
+/*
+    for (rowCounter; rowCounter >= 0; rowCounter--){
+
+        for(starCounter; starCounter >= 0; starCounter++){
+            cout<<"*";
+        }
+
+        cout<<endl;
+
+        starCounter = userInteger - rowCounter;
+
+    }
+
+*/
     return 0;
 }
