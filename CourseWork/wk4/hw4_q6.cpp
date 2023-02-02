8a2fad0cd1a01320d545beeca6ef4d744b29b59c
Leah Grace <Leahgdg@hotmail.com>
2023-02-01 21:30:44 -0500

    preliminary solution, testing needed

--- CourseWork/wk4/hw4_q6.cpp
+++ CourseWork/wk4/hw4_q6.cpp
@@ -10,10 +10,11 @@ const int EVEN_INTEGER_DIVISOR = 2;
 
 int main(){
 
-    int userInteger;
+    int userInteger; // Must remain unchanged
 
     cout<<"Enter a positive integer"<<endl;
     cin>>userInteger;
+
 
     //for each digit (for loop where x increments from 1 to userInteger)
     //find the number of digits in the number (use mod % 10 ?)
@@ -23,23 +24,56 @@ int main(){
     //compare the length of the two strings (best not to use .length)
     //print the number if there are more even digits than odd digits
 
-        int oddDigits = 0;
+    int counter = 1;
+    for (counter; counter < userInteger; counter++){ //iterate from 1 to n
+
+
+        //count digits
+        int oddDigits = 0; 
         int evenDigits = 0;
 
-        int currentInteger = userInteger;
 
+        //Evaluate the current integer from 1 - n using value from counter
+        int currentInteger = counter;
+
+        //Iterate over the digits in the current integer from 1 - n 
         while(currentInteger > 0) {
 
+           // cout<<"current integer "<<currentInteger<<endl;
+
+            //find the current digit with % 10
             int currentDigit = currentInteger % 10;
 
-            cout<<"Evaluate for even or odd "<<currentDigit<<endl;
-            cout<<"Progress while loop with "<<currentInteger / 10<<endl;
+         //   cout<<"current Digit "<<currentDigit<<endl;
 
+            //evaluate the current digit for even or odd
+            if (currentDigit % 2 == 0){
+                evenDigits++;
+            } else{
+                oddDigits++;
+            }
+
+           // cout<<"Evaluate for even or odd "<<currentDigit<<endl;
+           // cout<<"Progress while loop with "<<currentInteger / 10<<endl;
+
+            //remove the least significant digit of the current integer
             currentInteger /= 10;
 
-            cout<<currentInteger<<endl;
-            
         }
+
+        // compare the count of even and odd digits
+        // if there are more even digits than odd digits, print the original integer
+        if (evenDigits > oddDigits){
+            cout<<counter<<" has more even digits "<<endl;
+        }
+
+        //reassign even/odd counters back to 0
+        evenDigits = 0;
+        oddDigits = 0;
+
+       // current integer is 0 and must change to counter
+
+    }
 
     
 
