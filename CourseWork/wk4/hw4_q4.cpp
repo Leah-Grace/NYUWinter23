b3c71207159f43dd5c68a778c91f91e936247533
Leah Grace <Leahgdg@hotmail.com>
2023-02-02 17:41:07 -0500

    initial commit

---
+++ CourseWork/wk4/hw4_q4.cpp
@@ -0,0 +1,26 @@
+//this program prompts the user for a sequence of positive integers
+//the program calculates and prints the geometric mean 
+
+//first version:
+//prompts the user for the length of the sequence
+//prompts the user for the sequence
+//calculates and prints the geometric mean
+
+//Second version
+//Prompts the user to enter positive intgers until the sequence is ended with -1
+//calculates and prints the geometric mean
+
+//CALCULATIONS
+//The dataset will contain n positive integers
+//each intger is multiplied together
+//the nth root of the product is calculated
+//the result is returned as the geometric mean
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    return 0;
+}
+
