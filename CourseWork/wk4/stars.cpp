668e9c9b107c9524550fd805bd549235a79513c8
Leah Grace <Leahgdg@hotmail.com>
2023-02-02 22:07:47 -0500

    first commit, second attempt

---
+++ CourseWork/wk4/stars.cpp
@@ -0,0 +1,24 @@
+//This program will prompt the user for a positive integer, n
+//the program will print a textual image of an hourglass made of 2n lines of asterisks
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int userInteger = 0;
+    cout<<"Enter a positive integer"<<endl;
+    cin>>userInteger;
+
+    int rows = userInteger;
+    int starCounter = rows;
+    int spaces = 0;
+
+    while (rows > 0 ){
+
+    }
+
+    return 0; 
+}
