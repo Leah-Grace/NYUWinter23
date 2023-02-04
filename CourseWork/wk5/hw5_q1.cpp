030a5314aaaf37df45bebe2ae9fefa0f2a9628dc
Leah Grace <Leahgdg@hotmail.com>
2023-02-04 08:45:40 -0500

    solved

--- CourseWork/wk5/hw5_q1.cpp
+++ CourseWork/wk5/hw5_q1.cpp
@@ -12,8 +12,25 @@ using namespace std;
 
 int main(){
 
+    int userInteger = 0;
 
+    cout<<"Please enter a positive integer:"<<endl;
+    cin>>userInteger;
     
+    // Incriment over each row
+    for (int i = 1; i <= userInteger; i++){
+
+        // Incriment over each column
+        for (int j = 1; j <= userInteger; j++){
+            
+            //Print each multiple
+            cout<<(i * j)<<"\t";
+
+        }
+
+        cout<<endl;
+
+    }
 
     return 0;
 }
