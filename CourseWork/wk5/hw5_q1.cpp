29b595533cdbb4031a465e2dc1622f6f2dd61cb4
Leah Grace <Leahgdg@hotmail.com>
2023-02-04 08:45:16 -0500

    hw5 q1 initial commit

---
+++ CourseWork/wk5/hw5_q1.cpp
@@ -0,0 +1,19 @@
+/*
+Write a program that reads a positive integer n from the user and prints out a nxn
+multiplication table. The columns should be spaced by a tab.
+Your program should interact with the user exactly as it shows in the following example:
+Please enter a positive integer:
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    
+
+    return 0;
+}
